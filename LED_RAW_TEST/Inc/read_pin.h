7cf285ca47aaa96de11b6f36ed21c6544561a3cf
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-02-05 20:16:16 -0300

    structured code to be more modular. Missing development on reading PA2

---
+++ LED_RAW_TEST/Inc/read_pin.h
@@ -0,0 +1,14 @@
+/*
+ * read_pin.h
+ *
+ *  Created on: Feb 5, 2025
+ *      Author: alessandro.nardinelli
+ */
+
+#ifndef READ_PIN_H_
+#define READ_PIN_H_
+
+
+void readPA2State(void);
+
+#endif /* READ_PIN_H_ */
