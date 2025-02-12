5f1587bee407413972fb0082cfbeb801764fc723
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-02-11 21:16:12 -0300

    added comment and code logic where  pin PC0 MUST to be pulled low

--- LED_RAW_TEST/Inc/init.h
+++ LED_RAW_TEST/Inc/init.h
@@ -14,13 +14,13 @@
 
 void init(void);
 
-static int32_t *pGPIOC_CLK_EN = (int32_t *) 	0x40023830; // address for RCC_ AHB1ENR
-static int32_t  *pGPIOC_MODE_IN = (int32_t *) 	0x40020800; // Address for GPIOC_MODER
-static int32_t  *pGPIOC_INDR = (int32_t *) 		0x40020810; // Address for GPIOC_IDR (input data register)
+static volatile int32_t *pGPIOC_CLK_EN = (int32_t *) 	0x40023830; // address for RCC_ AHB1ENR
+static volatile int32_t  *pGPIOC_MODE_IN = (int32_t *) 	0x40020800; // Address for GPIOC_MODER
+static volatile int32_t  *pGPIOC_INDR = (int32_t *) 		0x40020810; // Address for GPIOC_IDR (input data register)
 
-static int32_t *pGPIOA_CLK_EN = (int32_t *) 	0x40023830; // address for RCC_ AHB1ENR
-static int32_t *pGPIOA_MODE_OUT = (int32_t *) 	0x40020000; // Address for GPIOA_MODER
-static int32_t *pGPIOA_DATA_OUT = (int32_t *) 	0x40020014; // Address for GPIOA_ODR (output data register)
+static volatile int32_t *pGPIOA_CLK_EN = (int32_t *) 	0x40023830; // address for RCC_ AHB1ENR
+static volatile int32_t *pGPIOA_MODE_OUT = (int32_t *) 	0x40020000; // Address for GPIOA_MODER
+static volatile int32_t *pGPIOA_DATA_OUT = (int32_t *) 	0x40020014; // Address for GPIOA_ODR (output data register)
 
 
 #endif /* INIT_H_ */
