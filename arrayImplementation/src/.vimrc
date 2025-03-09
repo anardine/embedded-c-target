b4d89d44d21bfa9ec16d1618020f7047d91bef5d
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-03-08 22:57:28 -0300

    fixing array implementation

---
+++ arrayImplementation/src/.vimrc
@@ -0,0 +1,14 @@
+syntax on
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set autoindent
+set smartindent
+set termguicolors
+set number
+colorscheme evening
+set mouse=a
+
+
+
+
--- arrayImplementation/src/main.c
+++ arrayImplementation/src/main.c
@@ -9,18 +9,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void swapArrays(int32_t const array1Addr, int32_t const array2Addr);
+void swapArrays(int32_t const *arrayAddr);
 int32_t* fetchArrayFromUser(void);
 
 
 int main(void) {
 
-	int32_t *userArrays = fetchArrayFromUser();
+	int32_t *pToUserArrays = fetchArrayFromUser();
 
+	printf("%p\n", pToUserArrays);
+	printf("%p\n", pToUserArrays+1);
 
-	printf("%p\n", userArrays);
-	printf("%p\n", userArrays+1);
-	//swapArrays(userArrays[0], userArrays[1]);
+	swapArrays(pToUserArrays);
 
 	return 0;
 }
@@ -44,7 +44,7 @@ int32_t* fetchArrayFromUser(void){
 		scanf("%d", &array1[i]);
 	}
 
-	for(int32_t i = 0; i < n1; i++){
+	for(int32_t i = 0; i < n2; i++){
 		printf("please enter the number for the %d position of the second array", i);
 		scanf("%d", &array2[i]);
 	}
@@ -52,14 +52,52 @@ int32_t* fetchArrayFromUser(void){
 	int32_t *memAddr1 = (int32_t*) &array1;
 	int32_t *memAddr2 = (int32_t*) &array2;
 
-	int32_t returnedArray[] = {*memAddr1, *memAddr2};
+	int32_t *pToquant1 = &n1;
+	int32_t *pToquant2 = &n2;
+
+	int32_t returnedArray[] = {*memAddr1, *memAddr2, *pToquant1, *pToquant2};
 
 	return (int32_t*) returnedArray;
 
 }
 
 
-void swapArrays(int32_t const array1Addr, int32_t const array2Addr) {
+void swapArrays(int32_t const *arrayAddr) {
 
+	int32_t *array1StartOffeset = arrayAddr;
+	int32_t *array2StartOffeset = arrayAddr+1;
+
+	int32_t array1Size = *(arrayAddr+2);
+	int32_t array2Size = *(arrayAddr+3);
+
+	int32_t newArray1[array2Size];
+
+	for (int32_t i = 0; i< array2Size; i++) {
+
+		newArray1[i] = array2StartOffeset[i];
+
+	}
+
+	int32_t newArray2[array1Size];
+
+	for (int32_t i = 0; i< array1Size; i++) {
+
+		newArray2[i] = array1StartOffeset[i];
+
+	}
+
+	printf("the new elements of array one are:");
+	for (int32_t i = 0; i< array2Size; i++) {
+
+		printf("%d\t",newArray1[i]);
+
+	}
+
+	printf("the new elements of array two are:");
+	for (int32_t i = 0; i< array1Size; i++) {
+
+		printf("%d\t",newArray2[i]);
+
+	}
 
 }
